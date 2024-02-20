d5e23fd2d11b8a1928cbdbf8dec789ff82d2ec47
Michal Massachi <michal2792000@gmail.com>
2024-02-20 21:17:42 +0200

    changes

--- app/src/main/java/com/example/dogsandddapters/Models/FirebasePersonPostModel.kt
+++ app/src/main/java/com/example/dogsandddapters/Models/FirebasePersonPostModel.kt
@@ -9,43 +9,43 @@ import com.google.firebase.ktx.Firebase
 
 class FirebasePersonPostModel {
 
-    private val db = Firebase.firestore
-
-    companion object {
-        fun getAllPersonPosts(since: Long, callback: (List<PersonPost>) -> Unit{
-            db.collection(PERSONPOST_COLLECTION_PATH)
-                .whereGreaterThanOrEqualTo(PersonPost.LAST_UPDATED, Timestamp(since, 0))
-                .get().addOnCompleteListener {
-                    when (it.isSuccessful) {
-                        true -> {
-                            val personPosts: MutableList<PersonPost> = mutableListOf()
-                            for (json in it.result) {
-                                val personPost = PersonPost.fromJSON(json.data)
-                                if (personPost != null) {
-                                    personPosts.add(personPost)
-                                }
-
-                            }
-                            callback(personPosts)
-                        }
-                        false -> callback(listOf())
-                    }
-                }
-        }
-
-    init {
-        val settings = firestoreSettings {
-            setLocalCacheSettings(memoryCacheSettings {  })
-        }
-        db.firestoreSettings = settings
-    }
-        fun addPersonPost(personPost: PersonPost, callback: () -> Unit) {
-            db.collection(PERSONPOST_COLLECTION_PATH).document(personPost.postid).set(personPost.toJson).addOnSuccessListener {
-                callback()
-            }
-        }
-
-        const val PERSONPOST_COLLECTION_PATH = "personposts"
-    }
+//    private val db = Firebase.firestore
+//
+//    companion object {
+//        fun getAllPersonPosts(since: Long, callback: (List<PersonPost>) -> Unit{
+//            db.collection(PERSONPOST_COLLECTION_PATH)
+//                .whereGreaterThanOrEqualTo(PersonPost.LAST_UPDATED, Timestamp(since, 0))
+//                .get().addOnCompleteListener {
+//                    when (it.isSuccessful) {
+//                        true -> {
+//                            val personPosts: MutableList<PersonPost> = mutableListOf()
+//                            for (json in it.result) {
+//                                val personPost = PersonPost.fromJSON(json.data)
+//                                if (personPost != null) {
+//                                    personPosts.add(personPost)
+//                                }
+//
+//                            }
+//                            callback(personPosts)
+//                        }
+//                        false -> callback(listOf())
+//                    }
+//                }
+//        }
+//
+//    init {
+//        val settings = firestoreSettings {
+//            setLocalCacheSettings(memoryCacheSettings {  })
+//        }
+//        db.firestoreSettings = settings
+//    }
+//        fun addPersonPost(personPost: PersonPost, callback: () -> Unit) {
+//            db.collection(PERSONPOST_COLLECTION_PATH).document(personPost.postid).set(personPost.toJson).addOnSuccessListener {
+//                callback()
+//            }
+//        }
+//
+//        const val PERSONPOST_COLLECTION_PATH = "personposts"
+//    }
 
 }
