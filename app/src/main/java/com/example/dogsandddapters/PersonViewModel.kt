3841472aeea3397735e2585f9f515a96dfbe28c3
Michal Massachi <michal2792000@gmail.com>
2024-03-05 20:12:03 +0200

    changes

---
+++ app/src/main/java/com/example/dogsandddapters/PersonViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.dogsandddapters
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.dogsandddapters.Models.Person
+
+class PersonViewModel : ViewModel(){
+    var person: LiveData<MutableList<Person>>? = null
+
+}
--- app/src/main/java/com/example/dogsandddapters/ProfileFragment.kt
+++ app/src/main/java/com/example/dogsandddapters/ProfileFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import com.example.dogsandddapters.Models.PersonModel
@@ -14,17 +15,20 @@ import com.example.dogsandddapters.Models.PersonModel
 class ProfileFragment : Fragment() {
 
     private val args: ProfileFragmentArgs by navArgs()
+    private lateinit var viewModel: PersonViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        viewModel = ViewModelProvider(this)[PersonViewModel::class.java]
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
         val email = args.email
 
