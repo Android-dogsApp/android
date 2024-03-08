f747b436342798779c4e1119a2e7c7193bca031e
Michal Massachi <michal2792000@gmail.com>
2024-03-08 19:59:58 +0200

    changes

--- app/src/main/java/com/example/dogsandddapters/dao/AppLocalDatabaseGeneralPost.kt
+++ app/src/main/java/com/example/dogsandddapters/dao/AppLocalDatabaseGeneralPost.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 import com.example.dogsandddapters.Models.GeneralPost
 import com.example.dogsandddapters.base.MyApplication
 
-@Database(entities = [GeneralPost::class], version = 5)
+@Database(entities = [GeneralPost::class], version = 6)
 abstract class AppLocalDbGPRepository : RoomDatabase() {
     abstract fun GeneralPostDao(): GeneralPostDao
 }
